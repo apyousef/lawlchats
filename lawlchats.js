4e39a45c850b97e076e97eea4feeff802002a94d
Andrew Werner <awerner32@gmail.com>
2012-08-04 14:08:05 -0700

    attempting to make socket.io work

--- lawlchats.js
+++ lawlchats.js
@@ -41,3 +41,12 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
   console.log("common = " + common.db_connection);
 });
+
+var io = require('socket.io').listen(app)
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
