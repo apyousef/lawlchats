5d4067b5f5f5fc900214dfabb7ab43dfb3ef1345
Andrew Werner <awerner32@gmail.com>
2012-08-04 14:14:08 -0700

    hermaphrodone socket.io

--- lawlchats.js
+++ lawlchats.js
@@ -37,12 +37,12 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
   console.log("common = " + common.db_connection);
 });
 
-var io = require('socket.io').listen(app)
+var io = require('socket.io').listen(server)
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
