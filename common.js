6606a6e7e8e7271648a54ff6b6bc1a86351e36fe
Robert Kwok <rob@crittercism.com>
2012-08-04 23:24:23 -0700

    added files to deploy to heroku

--- common.js
+++ common.js
@@ -1,6 +1,14 @@
-var mongoose = require('mongoose'),
-    redis = require('redis'),
-    redis_client = redis.createClient(6383, 'localhost');
+var mongoose = require('mongoose')
+var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+
+redis.set('foo', 'bar');
+
+redis.get('foo', function(err, value) {
+  console.log('foo is: ' + value);
+});
+
+    //redis = require('redis'),
+    //redis_client = redis.createClient(6383, 'localhost');
 
 var mongo_conn = mongoose.createConnection('mongodb://localhost:27017/lawlchatgoose');
 
