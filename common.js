8cb81ca79453949bf25b1c6414972c7b3baa223a
Robert Kwok <rob@crittercism.com>
2012-08-04 23:28:57 -0700

    fixing up heroku redis

--- common.js
+++ common.js
@@ -1,14 +1,8 @@
 var mongoose = require('mongoose')
-var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
-
-redis.set('foo', 'bar');
-
-redis.get('foo', function(err, value) {
-  console.log('foo is: ' + value);
-});
-
-    //redis = require('redis'),
-    //redis_client = redis.createClient(6383, 'localhost');
+var redis = require('redis-url')
+var redis_client = redis.connect(process.env.REDISTOGO_URL);
+//redis = require('redis'),
+//redis_client = redis.createClient(6383, 'localhost');
 
 var mongo_conn = mongoose.createConnection('mongodb://localhost:27017/lawlchatgoose');
 
